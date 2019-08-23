82d19887327bc917fa9fbc0bc3a5c3cfab3e17f9
yimsungbin <yimsb02@naver.com>
2019-08-23 15:10:46 +0900

    bitcamp-java-application V40 완료

---
+++ bitcamp-java-application4-server/v40/src/main/java/com/eomcs/lms/handler/PhotoBoardDeleteCommand.java
@@ -0,0 +1,54 @@
+package com.eomcs.lms.handler;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.eomcs.lms.dao.PhotoBoardDao;
+import com.eomcs.lms.dao.PhotoFileDao;
+import com.eomcs.util.ConnectionFactory;
+import com.eomcs.util.Input;
+
+public class PhotoBoardDeleteCommand implements Command {
+  
+  private ConnectionFactory conFactory;
+  private PhotoBoardDao photoBoardDao;
+  private PhotoFileDao photoFileDao;
+
+  public PhotoBoardDeleteCommand(ConnectionFactory conFactory, PhotoBoardDao photoBoardDao, PhotoFileDao photoFileDao) {
+    this.conFactory = conFactory;
+    this.photoBoardDao = photoBoardDao;
+    this.photoFileDao = photoFileDao;
+  }
+
+  @Override
+  public void execute(BufferedReader in, PrintStream out) {
+    Connection con = null;
+    try {
+      con = conFactory.getConnection();
+      con.setAutoCommit(false);
+      int no = Input.getIntValue(in, out, "번호?");
+
+      if (photoBoardDao.findBy(no) == null) {
+        out.println("해당 데이터가 없습니다.");
+        return;
+      }
+
+      // 먼저 게시물에 첨부파일을 삭제한다.
+      photoFileDao.deleteAll(no);
+
+      // 게시물을 삭제한다.
+      photoBoardDao.delete(no);
+      con.commit();
+      out.println("데이터를 삭제했습니다.");
+
+    } catch (Exception e) {
+      try {con.rollback();} catch (Exception e2) {}
+      out.println("데이터 삭제 실패!!");
+      System.out.println(e.getMessage());
+    } finally {
+      try {con.setAutoCommit(true);} catch (SQLException e) {}
+    }
+  }
+
+}
