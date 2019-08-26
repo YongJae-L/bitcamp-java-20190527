1372ed39ed16149ac53be517fdeaa5ca2cbeb53b
yimsungbin <yimsb02@naver.com>
2019-08-26 18:18:43 +0900

    bitcamp-java-application Mabatis

--- bitcamp-java-application/src/main/java/com/eomcs/lms/Main.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/Main.java
@@ -5,31 +5,39 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    br.readLine();
+    int N = Integer.parseInt(br.readLine());
     StringTokenizer st = new StringTokenizer(br.readLine());
-    while(st.hasMoreElements()) {
-      al.add(Long.parseLong(st.nextToken()));
-    }
-
-    for(int i=0;i<al.size();i++) {
-      boolean b = true;
-      int n=i;
-      while(n-->0) {
-        if(al.get(n)>al.get(i)) {
-          bw.write(n+1+" ");
-          b=false;
+    Stack<Long> stack = new Stack<>();
+    
+    stack.push(Long.parseLong(st.nextToken()));
+    bw.append("0");
+    int max = 1;
+    for(int i=1;i<N;i++) {
+      long now = Long.parseLong(st.nextToken());
+      while(!stack.isEmpty()) {
+        if(now<stack.peek()) {
+          max=i;
+          bw.append(" "+max);
+          stack.push(now);
           break;
+        } else {
+          stack.pop();
+          max--;
         }
       }
-      if(b)
-        bw.write(0+" ");
+      if(stack.isEmpty()) {
+        System.out.println("ㅇㅇ");
+        bw.write(" 0");
+        max=i+1;
+      }
+      stack.push(now);
     }
     bw.flush();
     br.close();
