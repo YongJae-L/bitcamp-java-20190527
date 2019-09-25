f977a3e9eac7b4fdb6004800fd7aba246c306652
yimsungbin <yimsb02@naver.com>
2019-09-25 18:31:00 +0900

    bitcamp-java-applicaion SPRING WEB MVC

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
@@ -1,11 +1,13 @@
-// v58_5 : request handler의 파라미터 값을 자동 주입하기
+// v59_1 : Spring WebMVC 적용하기
 package com.eomcs.lms;
 
 // 작업:
-// => request handler를 정의할 때 필요한 파라미터를 동적으로 선언한다.
-// => RequestMappingHandlerMapping 변경
-//
-//
+// => ContextLoaderListener 삭제
+// => DispatcherServlet 삭제
+// => RequestMappingHandlerMapping 삭제
+// => /WEB-INF/web.xml 변경
+
+
 // dummy 클래스!
 // => 기존 버전에서 계속 존재했던 클래스라서 그대로 둠.
 // => 단지 버전의 목표에 대한 설명을 기록하기 위해 존재함.
